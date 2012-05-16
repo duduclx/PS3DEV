a8dc056d1dedf3b3bad8a4e6ecfaea840090f335
dudu.clx <dudu.clx@hotmail.fr>
2012-05-16 16:20:10 +0200

    added norsx and ps-seismograph

---
+++ sources/norsx/README.md
@@ -0,0 +1,44 @@
+NoRSX is a 2D lib for PSL1GHT
+=============================
+
+This lib is unfinished. i hope to finish to write the text support soon.
+
+Don't be afraid to give me a help! this lib can be very useful!
+
+Authors
+-------
+	deroad
+	kakaroto (for his rsxutil.c)
+
+Changelog:
+----------
+	- 0.0.1 -
+		PNG/JPG support
+	- 0.0.2 -
+		Now it loads everything correctly.
+	- 0.0.3 -
+		Background support.
+		Can generate an object like rectangles
+	- 0.0.4 -
+		Added Msg dialog.
+		Added Sperimental Text (NO FONTS for now).
+		Added Partial Image function.
+	- 0.0.5 -
+		Added Printf to the Sperimental Text function.
+		Added Pixel.
+	- 0.0.5b -
+		Added Circle.
+		Added Pixel4.
+	- 0.0.5c -
+		Added AlphaBlending (15 fps).
+	- 0.0.6 -
+		Added line.
+	- 0.0.7 -
+		Added AlphaBlending (30 fps).
+
+
+Homebrews built with this lib:
+------------------------------
+	NoRSX Example  (deroad)
+	PS Seismograph (deroad)
+ 
---
+++ sources/norsx/source/main.cpp
@@ -0,0 +1,92 @@
+/*
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ This program was created by Grazioli Giovanni Dante <wargio@libero.it>.
+*/
+
+#include <io/pad.h>
+#include "NoRSX.h"
+#include "NoRSX_Image_bin.h"
+#include <time.h>
+
+static int exitapp, xmbopen;
+
+static inline void eventHandler(u64 status, u64 param, void * userdata)
+{
+	switch(status)
+	{
+		case SYSUTIL_EXIT_GAME:
+			exitapp = 0;
+			break;
+		case SYSUTIL_MENU_OPEN:
+			xmbopen = 1;
+			break;
+		case SYSUTIL_MENU_CLOSE:
+			xmbopen = 0;
+			break;
+	}
+}
+
+msgType MSG_OK = (msgType)(MSG_DIALOG_NORMAL | MSG_DIALOG_BTN_TYPE_OK | MSG_DIALOG_DISABLE_CANCEL_ON);
+
+s32 main(s32 argc, const char* argv[])
+{
+	padInfo padinfo ;
+	padData paddata ;
+	sysUtilRegisterCallback(SYSUTIL_EVENT_SLOT0, eventHandler, NULL);
+
+	ioPadInit(7);
+
+	pngData png;
+	
+	NoRSX *GFX = new NoRSX();
+	Image IMG(GFX);
+	Background BG(GFX);
+	Object OBJ(GFX);
+	Font F(GFX);
+	
+
+	IMG.LoadPNG_Buf(NoRSX_Image_bin,NoRSX_Image_bin_size, &png);
+	u32 imgX =(GFX->width/2)-(png.width/2), imgY = (GFX->height/2)-(png.height/2);
+	exitapp = 1;
+	int frame=0;
+	while(exitapp){
+		static time_t starttime = 0;
+		double fps = 0;
+		if (starttime == 0) starttime = time (NULL);
+		else fps = frame / difftime (time (NULL), starttime);
+		ioPadGetInfo(&padinfo);
+		if(padinfo.status[0]){
+			ioPadGetData(0, &paddata);
+			if(paddata.BTN_CROSS){
+				exitapp = 0;
+			}
+		}
+		BG.Mono(0xb4e83a); //a green hex color (you can use hex colors insted of COLOR_XXXXXXX)
+		OBJ.Circle(400,900,100,COLOR_YELLOW);
+		OBJ.Rectangle(500,400,200,500,COLOR_ORANGE);
+		OBJ.Line(400,400,900,100,COLOR_GREY);
+		IMG.AlphaDrawIMG(imgX,imgY,&png);
+		F.Printf(150,200,COLOR_BLACK,"SCREEN %d X %d",GFX->width,GFX->height);
+		F.Print(150,250,COLOR_YELLOW,"PRESS X TO EXIT");
+		F.Printf(150,100,COLOR_GREEN,"FPS %f", fps);
+		GFX->Flip();
+		frame ++;
+		sysUtilCheckCallback();
+	}
+	GFX->NoRSX_Exit();
+	ioPadEnd();
+	return 0;
+}
+
